1474ceddf153a4675bafef370e0cb2eae3384bb2
Omer <mrfkrtl89@gmail.com>
2022-07-05 21:56:42 +0300

    05/07/2022

--- src/test/java/Get02.java
+++ src/test/java/Get02.java
@@ -1,4 +1,9 @@
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertTrue;
 
 public class Get02 {
     /*
@@ -23,8 +28,22 @@ public class Get02 {
 
         //1. Step  Set the Url
 
-        String url ="https://restful-booker.herokuapp.com/booking/1001";
+        String url ="https://restful-booker.herokuapp.com/booking/1005";
 
+        //2. Step: Set the expected date(Post-Put-Patch)
+
+        //3.Step: Type code  to send request
+
+        Response response=given().when().get(url);
+        response.prettyPrint();
+
+        //4.Do Assertion
+
+        response.then().assertThat().statusCode(404).statusLine("HTTP/1.1 404 Not Found");
+
+        //Response body'de bulunan spesifik bir veri nasil assert edilir.
+
+        Assert.assertTrue(response.asString().contains("Not Found"));
 
     }
 }
