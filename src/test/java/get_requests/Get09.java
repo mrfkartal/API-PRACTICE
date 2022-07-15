f13e7a0ff87de200238a9f7d18b7dc39720d304f
Omer <mrfkrtl89@gmail.com>
2022-07-15 17:10:03 +0300

    15/07/2022

---
+++ src/test/java/get_requests/Get09.java
@@ -0,0 +1,31 @@
+package get_requests;
+
+import base_urls.HerOkuAppBaseUrl;
+
+public class Get09 extends HerOkuAppBaseUrl {
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking/91
+        When
+            I send GET Request to the url
+        Then
+            Response body should be like that;
+      {
+        "firstname": "James",
+        "lastname": "Brown",
+        "totalprice": 111,
+        "depositpaid": true,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+      }
+     */
+
+
+
+
+
+
+}
