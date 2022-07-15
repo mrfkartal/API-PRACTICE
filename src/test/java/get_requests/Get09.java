2a1908a364d8c7a38d699e33d1a4f2fee20d7848
Omer <mrfkrtl89@gmail.com>
2022-07-15 17:40:31 +0300

    15/07/2022

--- src/test/java/get_requests/Get09.java
+++ src/test/java/get_requests/Get09.java
@@ -1,6 +1,14 @@
 package get_requests;
 
 import base_urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Get09 extends HerOkuAppBaseUrl {
     /*
@@ -22,6 +30,47 @@ public class Get09 extends HerOkuAppBaseUrl {
         "additionalneeds": "Breakfast"
       }
      */
+    @Test
+    public void get01(){
+        //1. Step: Set the Url
+        spec.pathParams("first","booking","second",91);
+
+        //2. Step: Set the expected data
+
+        Map<String, String> bookingdatesMap = new HashMap<>();
+        bookingdatesMap.put("checkin","2022-07-14");
+        bookingdatesMap.put("checkout","2022-07-23");
+
+        Map<String,Object> expectedDataMap = new HashMap<>();
+        expectedDataMap.put("firstname","\"Rebekka\"");
+        expectedDataMap.put("lastname","\"Einvik\"");
+        expectedDataMap.put("totalprice",472);
+        expectedDataMap.put("depositpaid",true);
+        expectedDataMap.put("bookingdates",bookingdatesMap);
+        expectedDataMap.put("additionalneeds","\"Newspaper\"");
+
+        System.out.println(expectedDataMap);
+
+        //3. Step: Send the request and get the response
+
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+
+        Map<String, Object> actualDataMap = response.as(HashMap.class);
+        System.out.println(actualDataMap);
+
+
+        //4. Step: Do Assertion
+
+        assertEquals(expectedDataMap.get("firstname"),actualDataMap.get("firstname"));
+        assertEquals(expectedDataMap.get("lastname"),actualDataMap.get("lastname"));
+        assertEquals(expectedDataMap.get("depositpaid"),actualDataMap.get("depositpaid"));
+        assertEquals(expectedDataMap.get("totalprice"),actualDataMap.get("totalprice"));
+        assertEquals(expectedDataMap.get("additionalneeds"),actualDataMap.get("additionalneeds"));
+
+        assertEquals(bookingdatesMap.get("checkin"),((Map)actualDataMap.get("bookingdates")).get("checkin"));
+        assertEquals(bookingdatesMap.get("checkout"),((Map)actualDataMap.get("bookingdates")).get("checkout"));
+
+    }
 
 
 
