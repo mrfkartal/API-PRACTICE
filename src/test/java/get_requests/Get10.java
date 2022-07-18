70771cbb9470dc96941b937bc04638c7c17f62c7
Omer <mrfkrtl89@gmail.com>
2022-07-18 16:27:08 +0300

    18/07/2022

--- src/test/java/get_requests/Get10.java
+++ src/test/java/get_requests/Get10.java
@@ -1,6 +1,15 @@
 package get_requests;
 
 import base_urls.GorestBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import test_data.GoRestTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Get10 extends GorestBaseUrl {
      /*
@@ -25,4 +34,29 @@ public class Get10 extends GorestBaseUrl {
    }
    }
      */
+
+    @Test
+    public void get01(){
+        //1. Step: Set the Url
+        spec.pathParams("first","users","second",2965);
+
+        //2. Step: Set the expected data
+        GoRestTestData dataKey = new GoRestTestData();//Gerekli methodun çağrılması için obje oluşturuyorum.
+        Map<String , String> dataKeyMap = dataKey.dataKeyMap("Mr. Gita Menon", "gita_menon_mr@bayer.com","female","inactive");//İç Map'i oluşturuyorum.
+        Map<String , Object> expectedData = dataKey.expectedDataMap(null,dataKeyMap);//Üst Map'i oluşturan method.
+
+
+        //3. Step: Send the request and Get the Response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+
+        Map<String, Object> actualDataMap = response.as(HashMap.class);//De-Serialization==> Json formatından Java Objesine çevirme
+
+        //4. Step: Do Assertion
+        assertEquals(expectedData.get("meta"),actualDataMap.get("meta"));
+        assertEquals(dataKeyMap.get("name"),((Map)actualDataMap.get("data")).get("name"));//Önce "data" elementine ulaşıp buradan aldığım objeyi Map formatına cast ediyorum.
+        assertEquals(dataKeyMap.get("email"),((Map)actualDataMap.get("data")).get("email"));
+        assertEquals(dataKeyMap.get("gender"),((Map)actualDataMap.get("data")).get("gender"));
+        assertEquals(dataKeyMap.get("status"),((Map)actualDataMap.get("data")).get("status"));
+
+    }
 }
