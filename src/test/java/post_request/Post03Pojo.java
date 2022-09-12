69b9c90c930c121ac7fb03151cb90e0040392840
Omer <mrfkrtl89@gmail.com>
2022-09-12 20:35:37 +0300

    12/09/2022

--- src/test/java/post_request/Post03Pojo.java
+++ src/test/java/post_request/Post03Pojo.java
@@ -1,6 +1,13 @@
 package post_request;
 
 import base_urls.JsonplaceholderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.JsonPlaceHolderPojo;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Post03Pojo extends JsonplaceholderBaseUrl {
    //Pojo en cok tercih edilen yontemlerdendir.
@@ -25,5 +32,26 @@ public class Post03Pojo extends JsonplaceholderBaseUrl {
                                     }
      */
 
+    @Test
+    public void post01(){
+        //1. Step: Set the Url
+        spec.pathParam("first","todos");
 
+        //2. Step: Set the expected data
+        JsonPlaceHolderPojo payload = new JsonPlaceHolderPojo(55,"Tidy your room",false);
+        System.out.println("ExpectedData: "+payload);
+
+
+        //3. Step: Send Post Request and get the Response
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(payload).when().post("/{first}");
+        response.prettyPrint();
+
+        //4. Step: Do Assertion
+        JsonPlaceHolderPojo actualData = response.as(JsonPlaceHolderPojo.class);
+        System.out.println("actualData: "+actualData);
+        assertEquals(payload.getUserId(),actualData.getUserId());
+        assertEquals(payload.getTitle(),actualData.getTitle());
+        assertEquals(payload.getCompleted(),actualData.getCompleted());
+        assertEquals(201,response.getStatusCode());
+    }
 }
